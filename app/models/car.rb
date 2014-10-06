2fe20ba243f3db1155fab1b73e41ad6e1cf32b53
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-10-06 00:14:59 -0400

    added methods to car controller

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,7 @@ class Car < ActiveRecord::Base
   belongs_to :manufacturer
 
   validates :manufacturer_id, presence: true
+  validates :color, presence: true
+  validates :mileage, numericality: true, presence: true
+  validates :year, numericality:  {greater_than: 1920}, presence: true
 end
